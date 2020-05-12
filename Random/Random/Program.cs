c7d46bab2e8f19a533eb9e69c664b03da9364e1c
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-05-12 13:11:38 -0700

    Random yes no maybe question

---
+++ Random/Random/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace RandomT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int numEyes;
+
+            for(int i =0; i < 10; i++)
+            {
+                numEyes = dice.Next(1, 7);
+                Console.WriteLine(numEyes);
+
+            }
+
+            Random YesNoMaybe = new Random();
+            int answerNum= YesNoMaybe.Next(1,4);
+
+            Console.WriteLine("Type your question here... and get an answer.");
+            string input = Console.ReadLine();
+            string word = "";
+            if (answerNum == 1)
+            {
+                word = "Yes";
+            }
+            else if (answerNum == 2)
+            {
+                word = "No";
+            }else
+            {
+                word = "Maybe";
+            }
+
+            
+            Console.WriteLine(word);
+
+
+
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
